11daed59af47e1546e37d5c88887e4c9ac34e78d
mohit-kumar-singh55 <mohitkumarsingh12344@gmail.com>
2025-12-11 13:14:49 +0900

    Update: updated main menu and its UI

--- Assets/Scripts/Misc/MainMenuController.cs
+++ Assets/Scripts/Misc/MainMenuController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,14 +10,19 @@ public class MainMenuController : MonoBehaviour
     [Header("Element Names")]
     [SerializeField] string mainMenu = "MainMenu";
     [SerializeField] string optionsMenu = "OptionsMenu";
+    [SerializeField] string instructionsMenu = "InstructionsUI";
     [SerializeField] string startButton = "StartButton";
     [SerializeField] string quitButton = "QuitButton";
     [SerializeField] string optionsButton = "OptionsButton";
+    [SerializeField] string instructionsButton = "InstructionsButton";
     [SerializeField] string backButton = "BackButton";
     [SerializeField] string unlimitedBulletsCheckbox = "UnlimitedBulletsToggle";
     [SerializeField] string fireTypeDropdownText = "FireType";
 
-    private VisualElement root;
+    private VisualElement _root;
+    private VisualElement _menuUI;
+    private VisualElement _optionsUI;
+    private VisualElement _instructionsUI;
 
     void Start()
     {
@@ -26,27 +32,34 @@ public class MainMenuController : MonoBehaviour
             return;
         }
 
-        // Access the root VisualElement
-        root = mainMenuUI.rootVisualElement;
+        // Access the _root VisualElement
+        _root = mainMenuUI.rootVisualElement;
 
         // fetch checkpoint toggle
-        Toggle unlimitedBulletsToggle = root.Q<Toggle>(unlimitedBulletsCheckbox);
+        Toggle unlimitedBulletsToggle = _root.Q<Toggle>(unlimitedBulletsCheckbox);
 
-        // resetting options menu
-        ShowOptions(false);
+        // ** setting up UIs **
+        _menuUI = _root.Q(mainMenu);
+        _optionsUI = _root.Q(optionsMenu);
+        _instructionsUI = _root.Q(instructionsMenu);
+
+        // resetting menu
+        Back();
 
         // ** adding listeners **
         GetButton(startButton).clicked += LoadNewGame;
         GetButton(quitButton).clicked += Quit;
-        GetButton(optionsButton).clicked += () => ShowOptions(true);
-        GetButton(backButton).clicked += () => ShowOptions(false);
+        GetButton(optionsButton).clicked += () => ShowUI(_optionsUI);
+        GetButton(instructionsButton).clicked += () => ShowUI(_instructionsUI);
+        List<Button> backButtons = GetButtons(backButton);
+        backButtons.ForEach(b => b.clicked += Back);
 
         // ** setting unlimitedBullets toggle **
         unlimitedBulletsToggle.value = PlayerPrefs.GetInt(UnlimitedBulletsManager.UNLIMITED_BULLETS_KEY, 1) == 1;    // 0=off, 1=on
         unlimitedBulletsToggle.RegisterValueChangedCallback(evt => PlayerPrefs.SetInt(UnlimitedBulletsManager.UNLIMITED_BULLETS_KEY, evt.newValue ? 1 : 0));
 
         // ** setting fire type dropdown **
-        DropdownField fireTypeDropdown = root.Q<DropdownField>(fireTypeDropdownText);
+        DropdownField fireTypeDropdown = _root.Q<DropdownField>(fireTypeDropdownText);
 
         fireTypeDropdown.index = PlayerPrefs.GetInt(FireTypeManager.FIRE_TYPE_KEY, 0);
         fireTypeDropdown.RegisterValueChangedCallback(evt =>
@@ -62,15 +75,30 @@ public class MainMenuController : MonoBehaviour
         PlayerPrefs.Save();
     }
 
-    Button GetButton(string name) => root.Q<Button>(name);
+    Button GetButton(string name) => _root.Q<Button>(name);
+    List<Button> GetButtons(string name) => _root.Query<Button>(name).ToList();
 
     public void LoadNewGame() => SceneLoader.LoadScene(2);
 
-    void ShowOptions(bool show)
+    void ShowUI(VisualElement ui)
     {
-        root.Q(mainMenu).style.display = show ? DisplayStyle.None : DisplayStyle.Flex;
-        root.Q(optionsMenu).style.display = show ? DisplayStyle.Flex : DisplayStyle.None;
+        _menuUI.style.display = DisplayStyle.None;
+        ui.style.display = DisplayStyle.Flex;
     }
 
-    public void Quit() => Application.Quit();
+    public void Back()
+    {
+        _instructionsUI.style.display = DisplayStyle.None;
+        _optionsUI.style.display = DisplayStyle.None;
+        _menuUI.style.display = DisplayStyle.Flex;
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
 }
--- Assets/Scripts/System/Audio/BGMPlayer.cs
+++ Assets/Scripts/System/Audio/BGMPlayer.cs
@@ -2,8 +2,17 @@ using UnityEngine;
 
 public class BGMPlayer : MonoBehaviour
 {
+    public static BGMPlayer Instance { get; private set; }
+
     void Awake()
     {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
         DontDestroyOnLoad(gameObject);
     }
 }
